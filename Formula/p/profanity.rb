8e0e52c2378389366657ccd45c5fc2dd11a5fb20
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-23 11:57:50 +0000

    profanity 0.16.0

--- Formula/p/profanity.rb
+++ Formula/p/profanity.rb
@@ -1,8 +1,8 @@
 class Profanity < Formula
   desc "Console based XMPP client"
   homepage "https://profanity-im.github.io"
-  url "https://profanity-im.github.io/tarballs/profanity-0.15.1.tar.gz"
-  sha256 "c1e28a849aa178fd1ec5f384b0f8bdd244bce453dc4ef7bb76d0830382fec304"
+  url "https://profanity-im.github.io/tarballs/profanity-0.16.0.tar.gz"
+  sha256 "1f2e36a081cd299173f1c12b64b1cef971063e67bf226fb3f7048f2e67bd6a70"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -42,6 +42,13 @@ class Profanity < Formula
     depends_on "libassuan"
     depends_on "libgpg-error"
     depends_on "terminal-notifier"
+  end
+
+  # Define `prefs_changes_print` outside of `prefs_changes`
+  # https://github.com/profanity-im/profanity/pull/2090
+  patch do
+    url "https://github.com/profanity-im/profanity/commit/c512cd5ca7f36fc7b2972ed3db6df97831ebd7af.patch?full_index=1"
+    sha256 "06e2578e92ea2c45260d549755c97bcbb8e5a1af8a45fa773131dda149c19306"
   end
 
   def install
